84a4c829b2592d4c5d40fe0d0b9e31119b6d6a85
Nouments <nomentsua@gmail.com>
2026-08-06 16:53:47 +0300

    implementation of cd as builtin only for absolute path this time

--- internal/const.go
+++ internal/const.go
@@ -5,12 +5,12 @@ type Command func(cmd []string)
 
 var builtins = map[string]Command{}
 
-func init() {
+func init(){
     builtins["echo"] = Echo
     builtins["type"] = Type
     builtins["pwd"] = Pwd
 	builtins["exit"] = Exit
-	builtins["cd"] = Cd 
+	builtins["cd"] = Cd
 }
 
 
