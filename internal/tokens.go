d000d9995c352522b9fe009eff33700269ac59ff
Nouments <nomentsua@gmail.com>
2026-08-19 19:40:32 +0300

    separation fo lexer-parser and tokenization

--- internal/tokens.go
+++ internal/tokens.go
@@ -1,2 +1,18 @@
 package internal
 
+type TokenType string
+
+
+//value of tokentype that we are using
+const (
+	Word        TokenType = "WORD"
+	Pipe        TokenType = "PIPE"
+	RedirectOut TokenType = "REDIRECT_OUT"
+	RedirectAppend TokenType = "RADIRECT_APPEND"
+)
+
+//struct token 
+type Token struct {
+	Value string
+	Type  string
+}
